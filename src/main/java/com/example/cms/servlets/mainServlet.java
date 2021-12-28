d0002158b1db75aa2958de953e0ea0951a3545d9
Danzo7 <aymenmiro@gmail.com>
2021-12-28 16:57:12 +0100

    :rocket: add servlet and some dynamic jsp .

--- src/main/java/com/example/cms/servlets/mainServlet.java
+++ src/main/java/com/example/cms/servlets/mainServlet.java
@@ -4,18 +4,19 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "mainServlet", value = "")
 public class mainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        System.out.println(request.getSession(false));
         response.sendRedirect(request.getContextPath() + "/login");
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+             //response.sendRedirect(request.getContextPath() + "/students");
 
     }
 }
